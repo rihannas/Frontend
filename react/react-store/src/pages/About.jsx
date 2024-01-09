b3e4237ece6e29307d79207f5d00a1eae41fb95f
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2024-01-09 19:53:05 +0300

    update about us

--- react/react-store/src/pages/About.jsx
+++ react/react-store/src/pages/About.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 const About = () => {
 	return (
 		<>
-			<h1>About page</h1>
+			<h1>About us</h1>
+			<p>
+				We are the new skin care that will actually make your skin care about
+				its appreance by force
+			</p>
+			<p>We were founded in 2050</p>
+			<p>For any question, contact us</p>
+			<Link to="/contact">
+				<button>Contact Us</button>
+			</Link>
 		</>
 	);
 };
