46ec33e761d7fc5bc7980d7f1d6c6341c20977a4
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2024-02-17 21:11:44 +0300

    nav component

--- tailwind/SheoCo/src/components/Nav.jsx
+++ tailwind/SheoCo/src/components/Nav.jsx
@@ -1,5 +1,6 @@
+import { hamburger } from '../assets/icons';
 import { headerLogo } from '../assets/images';
-
+import { navLinks } from '../constants';
 const Nav = () => {
   return (
     <>
@@ -16,7 +17,26 @@ const Nav = () => {
             />
             <h2 className='px-2 text-purple-300'>SheoCo</h2>
           </a>
-          <ul className='flex-1 flex justify-center items-center gap-16 maxlg:hidden'></ul>
+          <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+            {navLinks.map((item) => (
+              <li key={item.label}>
+                <a
+                  href={item.href}
+                  className='font-montserrat leading-normal text-lg text-slate-gray'
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className='hidden max-lg:block'>
+            <img
+              src={hamburger}
+              alt='Hamburger'
+              width={24}
+              height={24}
+            />
+          </div>
         </nav>
       </header>
     </>
