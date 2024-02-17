a521a51baa3c890057658c24c90d41fb24c391a6
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2024-02-17 14:03:48 +0300

    add comments

--- react/todoApp/src/components/TodoForm.jsx
+++ react/todoApp/src/components/TodoForm.jsx
@@ -1,14 +1,18 @@
 /* eslint-disable react/prop-types */
 import { useState } from 'react';
 
+//form components
 const TodoForm = ({ handlesetTodoList }) => {
+  // state for new todos
   const [todo, setTodo] = useState('');
 
   function handleSetTodo(e) {
+    // event hander for setting new to do
     setTodo(e.target.value);
   }
 
   function handleAddTodoList() {
+    // event handler that calls another event handler from parent to update the todoList state
     handlesetTodoList(todo);
   }
   return (
