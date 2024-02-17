013581ea8e29a20dd163a8120cc191c3b5961aad
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2024-02-17 14:01:08 +0300

    add comments

--- react/todoApp/src/components/TodoList.jsx
+++ react/todoApp/src/components/TodoList.jsx
@@ -2,21 +2,27 @@
 import { useState } from 'react';
 import TodoItem from './TodoItem';
 import TodoForm from './TodoForm';
-
+// Main parent component
 const TodoList = () => {
+  // state for managing todos
   const [todoList, setTodoList] = useState([{ todo: 'read', status: true }]);
+  // state for managing the add button
   const [clicked, setClicked] = useState(false);
+
   function handleClick() {
+    // event handler for the add button
     setClicked(!clicked);
   }
 
   function handlesetTodoList(todo) {
+    // event handler for creating new todos form a form
     setTodoList(() => {
       return [...todoList, { todo: todo, status: true }];
     });
   }
 
   function handleCheckbox(todo) {
+    // event handler for checking and unchecking the checkbox
     todoList.forEach((tododict) => {
       if (todo == tododict.todo) {
         tododict.status = !tododict.status;
@@ -24,7 +30,9 @@ const TodoList = () => {
       setTodoList([...todoList]);
     });
   }
+
   function handleDelTodo(todo) {
+    // event handler for del todos
     const newList = todoList.filter((tododict) => tododict.todo != todo);
 
     setTodoList(newList);
@@ -37,6 +45,7 @@ const TodoList = () => {
           {todoList.length ? (
             <>
               {todoList.map((todo) => {
+                // renders todos if uncompleted
                 if (todo.status == true) {
                   return (
                     <TodoItem
@@ -57,6 +66,7 @@ const TodoList = () => {
           {
             <>
               {todoList.map((todo) => {
+                // renders todos if completed
                 if (todo.status == false) {
                   return (
                     <TodoItem
