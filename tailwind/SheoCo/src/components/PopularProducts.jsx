557bbfef809e242c5bc891b8db2b11f3f45ffd54
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2024-02-28 20:22:44 +0300

    done with popular product sections

--- tailwind/SheoCo/src/components/PopularProducts.jsx
+++ tailwind/SheoCo/src/components/PopularProducts.jsx
@@ -1,5 +1,33 @@
+import { products } from '../constants';
+import PopularProductCard from './PopularProductCard';
+
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <section
+      id='products'
+      className='max-container max-sm:mt-12'
+    >
+      <div className='flex flex-col justify-start gap-5'>
+        <h2 className='text-4xl font-palanquin font-bold'>
+          Discover <span className='text-purple-600'>Popular</span> Products
+        </h2>
+        <p className='lg:max-w-lg mt-2 font-montserrat text-slate-gray'>
+          Top-notch quality and style with our sought-after selections. Discover
+          a world of comfort, design and value{' '}
+        </p>
+      </div>
+      <div className='mt-16 grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1'>
+        {products.map((product) => {
+          return (
+            <PopularProductCard
+              key={product.name}
+              {...product}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default PopularProducts;
